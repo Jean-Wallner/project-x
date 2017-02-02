82ea7fc842293e271073cb3fb01d9d3f0ac3796a
Jean <jean.wallner1@gmail.com>
2017-02-02 17:49:19 +0100

    livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp      = require('gulp'),
-    less      = require('gulp-less'),
-    rename    = require('gulp-rename'),
-    cleancss  = require('gulp-clean-css');
+var gulp       = require('gulp'),
+    less       = require('gulp-less'),
+    rename     = require('gulp-rename'),
+    cleancss   = require('gulp-clean-css'),
+    livereload = require('gulp-livereload');
 
 gulp.task('less', function () {
   return gulp.src([
@@ -11,12 +12,15 @@ gulp.task('less', function () {
   .pipe( gulp.dest('./.tmp/css') )
   .pipe( cleancss() )
   .pipe( rename({
-    extname: '.min.css'
+    suffix: '.min'
   }))
-  .pipe( gulp.dest('./dist/css') );
+  .pipe( gulp.dest('./dist/css') )
+  .pipe(livereload());
 });
 
 gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch('./*.php', livereload.reload);
   gulp.watch('./rcs/less/**/*.less', ['less']);
 });
 
--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link href="dist/css/home.min.css" rel="stylesheet" type="text/css"/>
     <title>page de test</title>
   </head>
   <body>
-    Hello ;)
-    <?php echo "ok" ?>
+    <header>
+      je change du contenu c'est magique
+    </header>
+    <span class="ok">de ouf</span>
   </body>
 </html>
